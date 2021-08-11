66fb074a5ecac5e72c6fd668b0e6079960c434a2
Sangwon <ddw02141@kaist.ac.kr>
2021-08-12 00:22:36 +0900

    Design Add and Search Words Data Structure

--- 2021-W31/211.py
+++ 2021-W31/211.py
@@ -1,32 +1,45 @@
+import collections
+
+
+class TrieNode:
+
+    def __init__(self):
+        self.child = collections.defaultdict(TrieNode)
+        self.isWord = False
+
+
 class WordDictionary:
 
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.trie = [{}, False]
+        self.root = TrieNode()
 
     def addWord(self, word: str) -> None:
-        trie = self.trie
-        words = [""]
+        node = self.root
         for w in word:
-            words = [existing + wOrDot for wOrDot in (w, ".") for existing in words]
-        for Word in words:
-            trie = self.trie
-            for i, w in enumerate(Word):
-                if w not in trie[0]:
-                    trie[0][w] = [{}, False]
-                trie[0][w][1] = trie[0][w][1] or (i == len(Word) - 1)
-                trie = trie[0][w]
+            node = node.child[w]
+        node.isWord = True
 
     def search(self, word: str) -> bool:
-        trie = self.trie
-        for w in word:
-            if w not in trie[0]:
-                return False
-            trie = trie[0][w]
-        return trie[1]
+        node = self.root
+        self.res = False
+        self.dfs(node, word)
+        return self.res
 
+    def dfs(self, node, word):
+        if not node:
+            return
+        if not word:
+            self.res = self.res or node.isWord
+            return
+        w = word[0]
+        if w == ".":
+            for child in node.child.values():
+                self.dfs(child, word[1:])
+        else:
+            self.dfs(node.child.get(w), word[1:])
 
 # Your WordDictionary object will be instantiated and called as such:
 # obj = WordDictionary()
