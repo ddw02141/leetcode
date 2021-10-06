6468e89484f5df78c3ecc1f399a1a51dcc0f9f62
Sangwon <ddw02141@kaist.ac.kr>
2021-10-07 05:16:52 +0900

    301. Remove Invalid Parentheses

---
+++ 2021-W39/301.py
@@ -0,0 +1,74 @@
+from typing import List
+from itertools import product, combinations
+
+
+class Solution:
+    def removeInvalidParentheses(self, s: str) -> List[str]:
+        left = s.count('(')
+        right = s.count(')')
+        # print("left",left,"right",right)
+        answers = set()
+        indicesWithLeft = [i for i, x in enumerate(s) if x == '(']
+        indicesWithRight = [i for i, x in enumerate(s) if x == ')']
+        possible_answers = set()
+        if left > right:
+            indicesCombi = combinations(indicesWithLeft, left - right)
+            for indicies in indicesCombi:
+                newS = s[:]
+                for index in indicies:
+                    newS = newS[:index] + " " + newS[index + 1:]
+                newS = newS.replace(" ", "")
+                # print("newS", newS)
+                possible_answers.add(newS)
+        elif right > left:
+            indicesCombi = combinations(indicesWithRight, right - left)
+            for indicies in indicesCombi:
+                newS = s[:]
+                for index in indicies:
+                    newS = newS[:index] + " " + newS[index + 1:]
+                newS = newS.replace(" ", "")
+                possible_answers.add(newS)
+        else:  # left == right
+            possible_answers.add(s)
+        # print("possible_answers", possible_answers)
+        for ss in possible_answers:
+            indicesWithLeft = [i for i, x in enumerate(ss) if x == '(']
+            indicesWithRight = [i for i, x in enumerate(ss) if x == ')']
+            for i in range(len(ss) // 2):
+                indicesCombiLeft = list(combinations(indicesWithLeft, i))
+                indicesCombiRight = list(combinations(indicesWithRight, i))
+                for indiciesL in indicesCombiLeft:
+                    newS = ss[:]
+                    for indexL in indiciesL:
+                        # print("indexL", indexL)
+                        newS = newS[:indexL] + " " + newS[indexL + 1:]
+                    # print("newS", newS)
+                    for indiciesR in indicesCombiRight:
+                        newNewS = newS[:]
+                        for indexR in indiciesR:
+                            # print("indexR", indexR)
+                            newNewS = newNewS[:indexR] + " " + newNewS[indexR + 1:]
+                        newNewS = newNewS.replace(" ", "")
+                        # print("newNewS", newNewS)
+                        if newNewS and self.isValid(newNewS):
+                            answers.add(newNewS)
+        if self.isValid(s):
+            answers.add(s)
+        if not answers:
+            return [s.replace("(", "").replace(")", "")]
+        maxLen = max(len(answer) for answer in answers)
+        return [answer for answer in answers if len(answer) == maxLen]
+
+    def isValid(self, s):
+        stack = list()
+        for ss in s:
+            if ss == "(":
+                stack.append("(")
+            elif ss == ")":
+                if not stack or stack[-1] != "(":
+                    return False
+                else:
+                    stack = stack[:-1]
+        if stack:
+            return False
+        return True
